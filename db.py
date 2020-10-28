eaf760ab7af4adf5fc1389edaf73295fff344ee3
LukeOkane99 <theofficiallukeokane@gmail.com>
2020-10-28 16:43:03 +0000

    Can now add movies to database

--- db.py
+++ db.py
@@ -1,19 +1,14 @@
 import sqlite3
 import get_ratings
 
-movie = 'tenet'
-year = '2020'
-imdb, metacritic = get_ratings.get_imdb_metacritic_ratings(movie, year)
-tomatometer, audience_score = get_ratings.get_rotten_tomatoes_ratings(movie, year)
-letterboxd= get_ratings.get_letterboxd_rating(movie, year)
-tmdb = get_ratings.get_tmdb_rating(movie, year)
-average = get_ratings.get_average_rating(imdb, metacritic, tomatometer, audience_score, letterboxd, tmdb)
+movie = 'Bob'
+release_year = '2005'
+#title, year, imdb, metascore, tomatometer, audience_score, letterboxd, tmdb, avg = get_ratings.get_all_ratings(movie, release_year)
 
 # create database if it doesn't already exist
 def create_db():
     # create and connect to database
     conn = sqlite3.connect('database.db')
-
     # create movies table
     conn.execute('CREATE TABLE Movies (MovieId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, Title TEXT NOT NULL, Year TEXT NOT NULL, Imdb_rating REAL, Metascore REAL, Tomatometer REAL, Audience_score REAL, Letterboxd_rating REAL, Tmdb_rating REAL, Average_rating REAL)')
     # create users table
@@ -22,27 +17,52 @@ def create_db():
 
 # add a new movie to the database if not present
 def add_movie_to_db(ttl, yr, imdb, metacritic, tomatometer, audience_score, letterboxd, tmdb, average):
-    try:
-        with sqlite3.connect('database.db') as conn:
-            conn.cursor().execute("INSERT INTO Movies (title, year, Imdb_rating, Metascore, Tomatometer, Audience_score, Letterboxd_rating, Tmdb_rating, Average_rating) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
-            (ttl, yr, imdb, metacritic, tomatometer, audience_score, letterboxd, tmdb, average))
+    with sqlite3.connect('database.db') as conn:
+        cur = conn.cursor()
+        # Check if movie record already exists in database
+        cur.execute('SELECT rowid FROM Movies WHERE Title = (?) AND Year = (?)', (ttl,year))
+        record = cur.fetchall()
+        if (len(record) != 0):
+            print("A record already exists for this Movie")
+        else:
+            try:
+                cur.execute('INSERT INTO Movies (title, year, Imdb_rating, Metascore, Tomatometer, Audience_score, Letterboxd_rating, Tmdb_rating, Average_rating) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                (ttl, yr, imdb, metacritic, tomatometer, audience_score, letterboxd, tmdb, average))
+                conn.commit()
+            except:
+                # undo insertion if issues
+                conn.rollback()
+                print("error when inserting movie record")
+            finally:
+                if conn:
+                    print("record added successfully")
 
-            conn.commit()
-            print("record added successfully")
-    except:
-        # undo insertion if issues
-        conn.rollback()
-        print("error when inserting movie record")
-    finally:
-        conn.close()
+# delete all movie records from Movies table
+def delete_all_movies():
+    with sqlite3.connect('database.db') as conn:
+        cur = conn.cursor()
+        cur.execute('DELETE FROM Movies')
+        conn.commit()
 
-#create_database()
-add_movie_to_db(movie, year, imdb, metacritic, tomatometer, audience_score, letterboxd, tmdb, average)
+# show all records in Movies table
+def get_all_movies():
+    with sqlite3.connect('database.db') as conn:
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM Movies")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
 
-with sqlite3.connect('database.db') as conn:
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM Movies")
-    rows = cur.fetchall()
+def get_movie(ttl, yr):
+    with sqlite3.connect('database.db') as conn:
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM Movies WHERE Title = (?) AND Year = (?)', (ttl,yr))
+        record = cur.fetchall()
+        if (len(record) != 0):
+            print("Record found")
+            return record[0]
+        else:
+            print("No record exists for this movie")
 
-    for row in rows:
-        print(row)
+#add_movie_to_db(title, year, imdb, metascore, tomatometer, audience_score, letterboxd, tmdb, avg)
+print(get_movie(movie, release_year))
--- get_ratings.py
+++ get_ratings.py
@@ -1,25 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
 from requests import get, Session
-from selenium import webdriver
-from selenium.webdriver.common.by import By 
-from selenium.webdriver.support.ui import WebDriverWait 
-from selenium.webdriver.support import expected_conditions as EC 
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
 import json
 import re
 import lxml
 import time
 import cchardet
 
-movie = "tenet"
-year = "2020"
-
 session = Session()
 
-def get_imdb_metacritic_ratings(movie, year):
+# scrape imdb and metascore for movie
+def get_imdb_metascore_ratings(movie, year):
     try:
         response = session.get(r"https://www.imdb.com/search/title/?title="+movie+"+&title_type=feature")
         if response.status_code == 200:
@@ -36,8 +27,9 @@ def get_imdb_metacritic_ratings(movie, year):
     except Exception as ex:
         print(str(ex))
     finally:
-        return float(imdb_rating), float(metacritic_rating)
+        return film_title, film_year, ((float(imdb_rating))*10), float(metacritic_rating)
 
+# scrape letterboxd rating for movie
 def get_letterboxd_rating(movie, year):
     try:
         response = session.get("https://letterboxd.com/search/films/"+movie+" "+year)
@@ -47,7 +39,7 @@ def get_letterboxd_rating(movie, year):
             for title in titles:
                 # Split string into substrings based on title and year
                 ttl,yr = title.text.rsplit(' ', 1)
-                if movie == ttl.lower() and yr in year:  
+                if movie.lower() == ttl.lower() and yr in year:  
                     review_endpoint = title.find('a').get('href')
                     break
     except Exception as ex:
@@ -63,8 +55,9 @@ def get_letterboxd_rating(movie, year):
         except Exception as ex:
             print(str(ex))
         finally:
-            return rating
+            return round((rating*20), 1)
 
+# scrape rotten tomatoes tomatometer and audience score ratings
 def get_rotten_tomatoes_ratings(movie, year):
     try:
         response = session.get(r"https://www.rottentomatoes.com/search?search="+movie)
@@ -82,6 +75,7 @@ def get_rotten_tomatoes_ratings(movie, year):
     finally:
         return float(tomatometer_score), float(audience_score)
 
+# scrape tmdb rating
 def get_tmdb_rating(movie, year):
     try:
         response = session.get('https://api.themoviedb.org/3/search/movie?api_key=35847ff7af4ea4159527ddec5fa4a2f4&query='+movie+'&year='+year)
@@ -93,51 +87,22 @@ def get_tmdb_rating(movie, year):
     except Exception as ex:
             print(str(ex))    
     finally:
-        return tmdb_rating
+        return (tmdb_rating*10)
 
+# calculate an average rating from all ratings
 def get_average_rating(imdb, metascore, letterboxd, tomatometer, audience_score, tmdb):
-    avg_rating = ((imdb*10)+metascore+(letterboxd*20)+tomatometer+audience_score+(tmdb*10)) / 6
+    avg_rating = (imdb+metascore+letterboxd+tomatometer+audience_score+tmdb) / 6
     avg_rating = round(avg_rating, 1)
     return avg_rating
 
-#imdb, metascore = get_imdb_metacritic_ratings(movie, year)
-#letterboxd = get_letterboxd_rating(movie, year)
-#tomatometer, audience_score = get_rotten_tomatoes_ratings(movie, year)
-#tmdb = get_tmdb_rating(movie, year)
-#print(get_average_rating(imdb, metascore, letterboxd, tomatometer, audience_score, tmdb))
+# return all ratings
+def get_all_ratings(movie, year):
+    title, year, imdb, metascore = get_imdb_metascore_ratings(movie, year)
+    tomatometer, audience_score = get_rotten_tomatoes_ratings(movie, year)
+    letterboxd = get_letterboxd_rating(movie, year)
+    tmdb = get_tmdb_rating(movie, year)
+    # Close session after all scraping has occurred
+    session.close()
+    avg = get_average_rating(imdb, metascore, tomatometer, audience_score, letterboxd, tmdb)
 
-"""
-def get_roger_ebert_rating(movie, year):
-    # Set headless broswer option for chrome driver
-    options = Options()
-    options.headless = True
-    driver = webdriver.Chrome('C:\\bin\\chromedriver.exe', options = options)
-
-    # Search for moie title and click on relevant review page.
-    search_url = r"https://www.rogerebert.com/search?utf8=r%E2%9C%93&q="+movie+" "+year
-    driver.get(search_url)
-    search_results = driver.find_elements_by_xpath("//a[@class='gs-title']")
-    string1 = movie + " movie review (" + year + ")"
-    string2 = movie + " movie review & film summary (" + year + ")"
-    for result in search_results:
-        if string1.lower() in result.text.lower() or string2.lower() in result.text.lower():
-            result.send_keys(Keys.ENTER) 
-            break 
-
-    # get movie star rating element          
-    bs_object = BeautifulSoup(driver.page_source, 'lxml')
-    driver.close()
-    star_container = bs_object.find_all('span', class_ = "star-rating _large")
-
-    star_rating = 0
-    percentage_rating = 0
-    # Determine star rating and convert to percentage
-    for star in star_container[0].contents:
-        if "star-full" in str(star):
-            star_rating += 1
-            percentage_rating += 25
-        elif "star-half" in str(star):
-            star_rating += 0.5
-            percentage_rating += 12.5 
-    return star_rating
-"""
+    return title, year, imdb, metascore, tomatometer, audience_score, letterboxd, tmdb, avg
